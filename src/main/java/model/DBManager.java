ece0234dbc9704aed5b9e252fb5616f12f37288a
Teddan23 <129292434+Teddan23@users.noreply.github.com>
2024-10-05 20:01:10 +0200

    Changes by Michel

--- src/main/java/model/DBManager.java
+++ src/main/java/model/DBManager.java
@@ -2,6 +2,7 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBManager {
     // Databasens URL (för SQLite)
@@ -9,11 +10,14 @@ public class DBManager {
     private static final String DB_URL = "jdbc:sqlite:C:/Theodors_grejor/Distribuerade system/Web_Shopping/mydatabase.db";
     // Metod för att skapa och returnera en anslutning
     public static Connection getConnection() {
-        System.out.println("Hi!");
+        //System.out.println("Hi!");
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(DB_URL);
             System.out.println("Anslutning till SQLite-databasen lyckades!");
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute("PRAGMA busy_timeout = 5000;"); // Timeout i millisekunder (5 sek)
+            }
         } catch (SQLException e) {
             System.out.println("Kunde inte ansluta till databasen: " + e.getMessage());
         }
